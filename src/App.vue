bc7cc21dcabc1f5c693a1c1d6f6e9983016b3b01
shokh001 <shohjahondilmurodov56@gmail.com>
2023-11-21 17:14:07 +0500

    lesson-1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>Counter : {{ counter }}</h1>
+    <button @click="onIncrement" class="btn btn-success">ADD</button>
+    <button @click="onDecrement" class="btn btn-danger">Minus</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+
+  methods: {
+    onIncrement() {
+      this.counter +=1
+    },
+    onDecrement() {
+      this.counter -=1
+    }
+  }
+}
+</script>
+
+<style></style>
